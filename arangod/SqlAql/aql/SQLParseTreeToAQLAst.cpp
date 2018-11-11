0d45691559418c024cca69a42aa15da9a20d76e4
adongre <avinash@ampool.io>
2018-11-11 23:39:07 +0530

    Some more code cleanup - 3

--- arangod/SqlAql/aql/SQLParseTreeToAQLAst.cpp
+++ arangod/SqlAql/aql/SQLParseTreeToAQLAst.cpp
@@ -15,34 +15,33 @@ namespace hsql {
         std::cout << "SQLParseTreeToAQLAst::generateAQLAST" << std::endl;
 
         auto ast = _parser->ast();
+        ast->scopes()->start(arangodb::aql::AQL_SCOPE_FOR);
 
+        // Create Node for Data Source STARTS
         arangodb::aql::AstNode *nodeDataSource = nullptr;
         auto const &resolver = _parser->query()->resolver();
         nodeDataSource = ast->createNodeDataSource(resolver, TRI_CHAR_LENGTH_PAIR(_statement->fromTable->getName()),
                                                    arangodb::AccessMode::Type::READ, true, false);
-
-        ast->scopes()->start(arangodb::aql::AQL_SCOPE_FOR);
-
         arangodb::aql::AstNode *variableNode = ast->createNodeVariable(TRI_CHAR_LENGTH_PAIR("ROW"), true);
         arangodb::aql::Variable *variable = static_cast<arangodb::aql::Variable *>(variableNode->getData());
-
         ast->addOperation(ast->createNodeFor(variable, nodeDataSource, nullptr));
+        // Create Node for Data Source Ends
 
         auto nodeObject = ast->createNodeObject();
         _parser->pushStack(nodeObject);
 
-        auto variableR = ast->scopes()->getVariable(TRI_CHAR_LENGTH_PAIR("ROW"), true);
-        if (variableR == nullptr) {
-            std::cerr << "variable does not exist" << std::endl;
-        }
         // create a reference to the variable
-        auto nodeRef = ast->createNodeReference(variableR);
+        auto nodeRef = ast->createNodeReference(variable);
         _parser->pushObjectElement(TRI_CHAR_LENGTH_PAIR("ROW"), nodeRef);
 
         auto nodeRef1 = static_cast<arangodb::aql::AstNode *>(_parser->popStack());
 
         auto nodeR2 = ast->createNodeReturn(nodeRef1);
         ast->addOperation(nodeR2);
+
+
+
+
         ast->scopes()->endNested();
 
     }
